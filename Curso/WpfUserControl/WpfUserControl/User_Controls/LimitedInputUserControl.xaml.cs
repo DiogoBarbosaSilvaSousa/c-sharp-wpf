b2edc4b0360a68dfb7851ab56278d05514f054fb
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-10-20 20:07:18 -0300

    Criando e usando um controle de usuário (UserControl)

---
+++ Curso/WpfUserControl/WpfUserControl/User_Controls/LimitedInputUserControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUserControl.User_Controls
+{
+    /// <summary>
+    /// Interaction logic for LimitedInputUserControl.xaml
+    /// </summary>
+    public partial class LimitedInputUserControl : UserControl
+    {
+        public LimitedInputUserControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private string _title = "Título"; 
+        public string Title { get { return _title; } set { _title = value; } }
+
+        public int MaxLength { get; set; }
+    }
+}
