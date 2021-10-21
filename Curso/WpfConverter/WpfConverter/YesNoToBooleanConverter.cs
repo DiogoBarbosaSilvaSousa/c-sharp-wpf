4f5842182703b2b5860f27579f3c5fa769827a22
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-10-21 01:11:24 -0300

    Conversão de valores com IValueConverter

---
+++ Curso/WpfConverter/WpfConverter/YesNoToBooleanConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace WpfConverter
+{
+    public class YesNoToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "yes":
+                case "oui":
+                case "sim":
+                    return true;
+                case "no":
+                case "non":
+                case "não":
+                    return false;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value == true)
+                    return "yes";
+                else
+                    return "no";
+            }
+            return "no";
+        }
+    }
+
+}
