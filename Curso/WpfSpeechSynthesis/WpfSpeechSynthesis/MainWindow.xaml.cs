bacd3361de9f339a3f73d50906cb0331570e4df6
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-10-27 00:04:12 -0300

    Sintetizador de fala (fazendo o WPF falar)

---
+++ Curso/WpfSpeechSynthesis/WpfSpeechSynthesis/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSpeechSynthesis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.Speak("Hello, world!");
+        }
+    }
+}
---
+++ Curso/WpfSpeechSynthesis/WpfSpeechSynthesisPromptBuilder/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSpeechSynthesisPromptBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, RoutedEventArgs e)
+        {
+            PromptBuilder promptBuilder = new PromptBuilder();
+            promptBuilder.AppendText("Olá mundo");
+
+            PromptStyle promptStyle = new PromptStyle();
+            promptStyle.Volume = PromptVolume.Soft;
+            promptStyle.Rate = PromptRate.Slow;
+            promptBuilder.StartStyle(promptStyle);
+            promptBuilder.AppendText("e olá para o universo também.");
+            promptBuilder.EndStyle();
+
+            promptBuilder.AppendText("Neste dia, ");
+            promptBuilder.AppendTextWithHint(DateTime.Now.ToShortDateString(), SayAs.Date);
+
+            promptBuilder.AppendText(", estamos reunidos aqui para aprender");
+            promptBuilder.AppendText("tudo", PromptEmphasis.Strong);
+            promptBuilder.AppendText("sobre");
+            promptBuilder.AppendTextWithHint("WPF", SayAs.SpellOut);
+
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.Speak(promptBuilder);
+        }
+    }
+}
